0bd62073a2bce2febc8d87a97bf1a494b064b8ad
Martin Kemp <martin@martinke.mp>
2016-03-09 17:08:18 +0000

    uwsgi expects application

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!venv/bin/python
 """Run script for the app."""
-from app import app
+from app import app as application
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=app.config["DEBUG"])
